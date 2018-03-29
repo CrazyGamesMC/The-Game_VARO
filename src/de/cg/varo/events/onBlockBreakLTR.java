703428fca3106bd8e5e87490891a7d68ded72039
CrazyGamesMC <37277385+CrazyGamesMC@users.noreply.github.com>
2018-03-29 11:07:29 +0200

    Update onBlockBreakLTR.java

--- src/de/cg/varo/events/onBlockBreakLTR.java
+++ src/de/cg/varo/events/onBlockBreakLTR.java
@@ -7,6 +7,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 
+import de.cg.varo.game.Methods;
 import de.cg.varo.game.Var;
 
 public class onBlockBreakLTR implements Listener{
@@ -20,7 +21,24 @@ public class onBlockBreakLTR implements Listener{
 			
 			e.setCancelled(true);
 			
+		
 			
+			
+			
+		} else {
+			
+			
+			if (e.getBlock().getType() == Material.CHEST) {
+				
+				if (Methods.chestIsLocked(e.getBlock().getLocation(), e.getPlayer())) {
+				
+					e.setCancelled(true);
+				
+					e.getPlayer().sendMessage(Var.prefix + "§cDiese Kiste ist gesichert!");
+				
+				} 
+				
+			}
 			
 		}
 		
@@ -48,6 +66,38 @@ public class onBlockBreakLTR implements Listener{
 			
 			e.setCancelled(true);
 			
+		} else {
+			
+			if (e.getBlock().getType() == Material.CHEST) {
+				
+				int size = 0; 
+				
+				if (Var.chests.getConfigurationSection("chests").getKeys(false) != null) {
+					
+					size = Var.chests.getConfigurationSection("chests").getKeys(false).size(); 
+					
+				}
+					
+					
+				size = Var.chests.getConfigurationSection("chests").getKeys(false).size(); 
+				
+				if (Var.teams.getBoolean("teams." + Methods.getTeam(e.getPlayer()) + ".lockChest")) {
+					
+					size++;
+					
+					Var.chests.set("chests.chest"+size+".X", e.getBlock().getLocation().getBlockX());
+					Var.chests.set("chests.chest"+size+".Y", e.getBlock().getLocation().getBlockY());
+					Var.chests.set("chests.chest"+size+".Z", e.getBlock().getLocation().getBlockZ());
+					Var.chests.set("chests.chest"+size+".team", Methods.getTeam(e.getPlayer()));
+					
+					
+					e.getPlayer().sendMessage(Var.prefix + "Teamkiste erstellt!");
+					
+					Methods.saveFile("chests");
+					
+				}
+				
+			}
 			
 			
 		}
